83a6c012bfeaf8f6eefad1184ccc2020d5d50c91
Lassi Kortela <lassi@lassi.io>
2018-10-28 15:31:58 +0200

    Handle Escape and other special keys

--- cursory/CursoryUnix.java
+++ cursory/CursoryUnix.java
@@ -15,9 +15,23 @@ public class CursoryUnix extends Cursory {
         static final String libc = "c";
         static { Native.register(libc); }
 
+        public static final short POLLIN = 1;
         public static final int NCCS = 20;
         public static final int TCSAFLUSH = 2;
         public static final int TIOCGWINSZ = 1074295912;
+
+        public static class PollfdStruct extends Structure {
+            protected List<String> getFieldOrder() {
+                return Arrays.asList(new String[] {
+                    "fd",
+                    "events",
+                    "revents",
+                });
+            }
+            public int fd;
+            public short events;
+            public short revents;
+        }
 
         public static class WinsizeStruct extends Structure {
             protected List<String> getFieldOrder() {
@@ -55,6 +69,8 @@ public class CursoryUnix extends Cursory {
             public long c_ospeed;
         }
 
+        public static native int poll(PollfdStruct pollfd, int nfds,
+                                      int timeout) throws LastErrorException;
         public static native int tcgetattr(int fd, TermiosStruct termios_p)
             throws LastErrorException;
         public static native int tcsetattr(int fd, int optional_actions,
@@ -65,6 +81,23 @@ public class CursoryUnix extends Cursory {
         public static native int ioctl(int fd, long request,
                                        WinsizeStruct ttysize_p)
             throws LastErrorException;
+    }
+
+    private static final int TIMEOUT_MS = 100;
+    private TermiosNative.PollfdStruct pollfd =
+        new TermiosNative.PollfdStruct();
+
+    private int readByte() throws Exception { return System.in.read(); }
+
+    private int readByteWithTimeout() throws Exception {
+        if (System.in.available() < 1) {
+            pollfd.fd = 0;
+            pollfd.events = TermiosNative.POLLIN;
+            if (TermiosNative.poll(pollfd, 1, TIMEOUT_MS) < 1) {
+                return -1;
+            }
+        }
+        return readByte();
     }
 
     public static boolean isatty(int fileDescriptor) throws Exception {
@@ -107,11 +140,66 @@ public class CursoryUnix extends Cursory {
         return new XY(ws.ws_col, ws.ws_row);
     }
 
+    private String readEscBracket() throws Exception {
+        int byt = readByteWithTimeout();
+        if ((byt >= '0') && (byt <= '9') && (readByteWithTimeout() != '~')) {
+            return null;
+        }
+        switch (byt) {
+        case '3':
+            return "delete";
+        case '5':
+            return "pageup";
+        case '6':
+            return "pagedown";
+        case 'A':
+            return "up";
+        case 'B':
+            return "down";
+        case 'C':
+            return "right";
+        case 'D':
+            return "left";
+        case 'H':
+            return "home";
+        case 'F':
+            return "end";
+        }
+        return null;
+    }
+
+    private String readEscO() throws Exception {
+        switch (readByteWithTimeout()) {
+        case 'H':
+            return "home";
+        case 'F':
+            return "end";
+        }
+        return null;
+    }
+
+    private String readEsc() throws Exception {
+        switch (readByteWithTimeout()) {
+        case '[':
+            return readEscBracket();
+        case 'O':
+            return readEscO();
+        case -1:
+            return "escape";
+        }
+        return null;
+    }
+
     public Event readEvent() throws Exception {
+        // TODO: Support fd's other than stdin.
         Event e = new Event();
-        int byt = System.in.read(); // TODO: Support fd's other than stdin.
+        int byt = readByte();
         if (byt == -1) {
             e.eventType = "";
+            e.which = null;
+        } else if (byt == 0x1b) {
+            e.eventType = "specialkey";
+            e.which = readEsc();
         } else {
             e.eventType = "char";
             e.which = new String(new byte[] {(byte)byt});
